4aca1046a79638f91437189a3fa3f5ddb7f9aaf3
thefool309 <themagician309@gmail.com>
2025-02-09 14:35:33 -0600

    idk if it works or not untested still

--- bash/compressHome.sh
+++ bash/compressHome.sh
@@ -5,32 +5,41 @@
 ### DON'T DO WHAT VELMA DOES AND sudo rm -rf $HOME
 set -x
 #prep filename
-$now = date -d "%m_%d_%Y_%H_%M"
+now=$(date +"%m_%d_%Y_%H_%M")
 
-$stageName = "HomeBackup_$now"
+stageName="HomeBackup_$now"
 
 
 # create the stage
 echo "creating stage $HOME/$stageName/"
-mkdir -p $HOME/$stageName/
+mkdir -p "$HOME/$stageName/"
 # copy all things you wish to save to stage
 echo "copying files..."
-cp -R $HOME/Archives/ $HOME/$stageName/
-cp -R $HOME/Downloads/ $HOME/$stageName/
-cp -R $HOME/Documents/ $HOME/$stageName/
-cp -R $HOME/.font_backups/ $HOME/$stageName/
-cp -R $HOME/Music/ $HOME/$stageName/
-cp -R $HOME/Pictures/ $HOME/$stageName/
-cp -R $HOME/Videos/ $HOME/$stageName/
-cp -R $HOME/.bash_profile $HOME/$stageName/
-cp -R $HOME/.gitconfig/ $HOME/$stageName/
-cp -R $HOME/.nv/ $HOME/$stageName/
-cp -R $HOME/.nvidia-settings-rc $HOME/$stageName/
+if [ -d "$HOME/Archives/" ]; then
+    cp -R "$HOME/Archives/" "$HOME/$stageName/"
+else
+    echo "No directory $HOME/Archives/ skipping..."
+fi
+if [ -d "$HOME/Downloads/" ]; then
+    cp -R "$HOME/Downloads/" "$HOME/$stageName/"
+else
+    echo "No directory $HOME/Downloads/ skipping..."
+fi
+
+cp -R "$HOME/Documents/" "$HOME/$stageName/"
+cp -R "$HOME/.font_backups/" "$HOME/$stageName/"
+cp -R "$HOME/Music/" "$HOME/$stageName/"
+cp -R "$HOME/Pictures/" "$HOME/$stageName/"
+cp -R "$HOME/Videos/" "$HOME/$stageName/"
+cp -R "$HOME/.bash_profile" "$HOME/$stageName/"
+cp -R "$HOME/.gitconfig/" "$HOME/$stageName/"
+cp -R "$HOME/.nv/" "$HOME/$stageName/"
+cp -R "$HOME/.nvidia-settings-rc" "$HOME/$stageName/"
 
 
 # compress stage into archive
 echo "beginning compression"
 
-echo tar -czvf $HOME/$stageName.tar.gz $HOME/$stageName/
+echo tar -czvf "$HOME/$stageName.tar.gz" "$HOME/$stageName/"
 # clean up by deleting stage
-# rm -rv $HOME/$stageName/
+# rm -ri $HOME/$stageName/
