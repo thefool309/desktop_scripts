9be906398cd0678096d6becb479eb7e435e9356f
thefool309 <themagician309@gmail.com>
2025-02-10 00:43:22 -0600

    fixed file pathing issues with variables. removed redundant statements causing filepath to be incorrectly parsed.

--- bash/sort_downloads.sh
+++ bash/sort_downloads.sh
@@ -10,7 +10,7 @@ now=$(date +"%m_%d_%Y_%H_%M_%S")
 filename="sortResults_$now.txt"
 
 # Ensure target directories exist
-if [ ! -e "./.sortResults/" ]; then
+if [ ! -e "$HOME/.sortResults/" ]; then
     echo "no sortResults folder making one now..."
     mkdir .sortResults/
 fi
@@ -55,33 +55,34 @@ if [ ! -d "$HOME/.font_backups/" ]; then
     mkdir -p "$HOME/.font_backups/"
 fi
 
-dirName="$HOME.sortResults"
+dirName="$HOME/.sortResults"
+echo "$dirName is the directory"
 # Move Videos
 #find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.mp4" -o -iname "*.mkv" \) -exec echo "Found: {}" \;
-find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.mp4" -o -iname "*.mkv" \) -exec echo mv {} $HOME/Videos/ \; > ./$dirName/$filename
+find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.mp4" -o -iname "*.mkv" \) -exec echo mv {} $HOME/Videos/ \; > $dirName/$filename
 find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.mp4" -o -iname "*.mkv" \) -exec mv {} $HOME/Videos/ \;
 # Move Music
-find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.mp3" -o -iname "*.flac" -o -iname "*.wav" -o -iname "*.aac" -o -iname "*.aiff" -o -iname "*.dsd" -o -iname "*.pcm" \) -exec echo mv {} $HOME/Music/SongsToSort/ \; > ./$dirName/$filename
+find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.mp3" -o -iname "*.flac" -o -iname "*.wav" -o -iname "*.aac" -o -iname "*.aiff" -o -iname "*.dsd" -o -iname "*.pcm" \) -exec echo mv {} $HOME/Music/SongsToSort/ \; > $dirName/$filename
 find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.mp3" -o -iname "*.flac" -o -iname "*.wav" -o -iname "*.aac" -o -iname "*.aiff" -o -iname "*.dsd" -o -iname "*.pcm" \) -exec mv {} $HOME/Music/SongsToSort/ \;
 # Move ISO files
-find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.iso" \) -exec echo mv {} $HOME/DiskImages/ \; >> ./$dirName/$filename 
+find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.iso" \) -exec echo mv {} $HOME/DiskImages/ \; >> $dirName/$filename 
 find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.iso" \) -exec mv {} $HOME/DiskImages/ \; 
 # Move Pictures
-find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.gif" -o -iname "*.webp" \) -exec echo mv {} $HOME/Pictures/ToBeSorted/ \; >> ./$dirName/$filename 
+find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.gif" -o -iname "*.webp" \) -exec echo mv {} $HOME/Pictures/ToBeSorted/ \; >> $dirName/$filename 
 find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.gif" -o -iname "*.webp" \) -exec mv {} $HOME/Pictures/ToBeSorted/ \;
 # move SVGs
-find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.svg" \) -exec echo mv {} $HOME/Pictures/SVGs/ \; >> ./$dirName/$filename 
+find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.svg" \) -exec echo mv {} $HOME/Pictures/SVGs/ \; >> $dirName/$filename 
 find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.svg" \) -exec mv {} $HOME/Pictures/SVGs/ \; 
 # Move archives
-find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.zip" -o -iname "*.7z" -o -iname "*.tar" -o -iname "*.tar.*" \) -exec echo mv {} $HOME/Archives/ \; >> ./$dirName/$filename
+find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.zip" -o -iname "*.7z" -o -iname "*.tar" -o -iname "*.tar.*" \) -exec echo mv {} $HOME/Archives/ \; >> $dirName/$filename
 find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.zip" -o -iname "*.7z" -o -iname "*.tar" -o -iname "*.tar.*" \) -exec mv {} $HOME/Archives/ \;
 # move documents
-find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.txt" -o -iname "*.pdf" -o -iname "*.doc" -o -iname "*.ppt" -o -iname "*.docx" -o -iname "*.pptx" \) -exec echo mv {} $HOME/Documents/ \; >> ./$dirName/$filename
+find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.txt" -o -iname "*.pdf" -o -iname "*.doc" -o -iname "*.ppt" -o -iname "*.docx" -o -iname "*.pptx" \) -exec echo mv {} $HOME/Documents/ \; >> $dirName/$filename
 find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.txt" -o -iname "*.pdf" -o -iname "*.doc" -o -iname "*.ppt" -o -iname "*.docx" -o -iname "*.pptx" \) -exec mv {} $HOME/Documents/ \;
 #move fonts
-find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.ttf" -o -iname "*.otf" -o -iname "*.woff" \) -exec echo mv {} $HOME/.font_backups/ \; >> ./$dirName/$filename
+find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.ttf" -o -iname "*.otf" -o -iname "*.woff" \) -exec echo mv {} $HOME/.font_backups/ \; >> $dirName/$filename
 find $HOME/Downloads/ -maxdepth 1 -type f \( -iname "*.ttf" -o -iname "*.otf" -o -iname "*.woff" \) -exec mv {} $HOME/.font_backups/ \;
  
 echo "operation complete!"
 echo "please check the file inside" 
-echo "./$dirName/$filename for your results"
+echo "$dirName/$filename for your results"
